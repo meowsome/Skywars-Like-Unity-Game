b6e13a97900f55c8b8241fe600bc5cfb1d05a207
meowsome <tycarr01@gmail.com>
2021-03-22 23:09:06 -0400

    If item is further than set distance from center, destroy item

--- Assets/Scripts/Game/Player/Inventory/ItemBehavior.cs
+++ Assets/Scripts/Game/Player/Inventory/ItemBehavior.cs
@@ -1,12 +1,14 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Mirror;
 
-public class ItemBehavior : MonoBehaviour {
+public class ItemBehavior : NetworkBehaviour {
     private int spinSpeed = 30;
     private Vector3 move;
     private float verticalVelocity;
     private float gravity = 50.0f;
+    private float destroyDistance = 500f;
 
     void Start() {
         move = new Vector3(0, 0, 0);
@@ -17,6 +19,7 @@ public class ItemBehavior : MonoBehaviour {
 
     void Update() {
         handleRotate();
+        checkIfTooFar();
     }
 
     private void handleRotate() {
@@ -29,5 +32,10 @@ public class ItemBehavior : MonoBehaviour {
 
     private bool isGrounded() {
         return Physics.Raycast(transform.position, transform.TransformDirection(Vector3.down), out RaycastHit hit, 1f);
+    }
+
+    // If dropped item is further than defined distance from the center of the map, destroy the item altogether
+    private void checkIfTooFar() {
+        if (Vector3.Distance(transform.position, new Vector3(0, 0, 0)) > destroyDistance) NetworkServer.Destroy(gameObject);
     }
 }
