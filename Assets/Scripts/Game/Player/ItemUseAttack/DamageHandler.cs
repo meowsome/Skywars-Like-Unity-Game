925d8ffccb86402d85f6ab262721b0b265807768
meowsome <tycarr01@gmail.com>
2021-05-23 20:48:32 -0400

    Glow object that was hit red for defined number of seconds

--- Assets/Scripts/Game/Player/ItemUseAttack/DamageHandler.cs
+++ Assets/Scripts/Game/Player/ItemUseAttack/DamageHandler.cs
@@ -8,6 +8,9 @@ using UnityEngine.UI;
 using UnityEditor;
 
 public class DamageHandler : NetworkBehaviour {
+    private float redHitDelay = 0.4f;
+    private Color redColor = Color.red;
+
     public void damagePlayer(string sender, string receiver, float amount) {
         if (isServer) {
             damagePlayerServer(sender, receiver, amount);
@@ -15,6 +18,8 @@ public class DamageHandler : NetworkBehaviour {
     }
 
     private void damagePlayerServer(string sender, string receiver, float amount) {
+        StartCoroutine(glowRed(receiver));
+
         // Track amount of damage dealt (server side)
         DamageTracker damageTracker = GameObject.FindWithTag("MapHandler").GetComponent<MapHandlerServer>().damageTracker;
         damageTracker.addDamage(sender, receiver, amount);
@@ -32,11 +37,23 @@ public class DamageHandler : NetworkBehaviour {
 
     [ClientRpc]
     private void damagePlayerClients(string sender, string receiver, float amount) {
+        StartCoroutine(glowRed(receiver));
+
         // Visually decrease health bar
         HealthBehavior healthBehavior = GameObject.Find("Health/Health Canvas/Health Bar").GetComponent<HealthBehavior>();
 
         if (healthBehavior.name == receiver) {
             healthBehavior.decreaseHealth(amount);
         }
+    }
+
+    // Make glow red for defined # of seconds
+    private IEnumerator glowRed(string receiver) {
+        GameObject receiverCharacterRepresentation = GameObject.Find(receiver + "/Cylinder"); // Get the game object
+        Color oldColor = receiverCharacterRepresentation.GetComponent<MeshRenderer>().material.color; // Keep track of old color
+
+        receiverCharacterRepresentation.GetComponent<MeshRenderer>().material.color = redColor; // Turn red
+        yield return new WaitForSeconds(redHitDelay); // Wait, delay for defined # of seconds
+        receiverCharacterRepresentation.GetComponent<MeshRenderer>().material.color = oldColor; // Fade from red to old color again
     }
 }
